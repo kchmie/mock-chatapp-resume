53321a70ad49e5dc1b55c3364c1e837f5a999f4c
chris <47784402+christhefrog@users.noreply.github.com>
2024-04-19 19:19:36 +0200

    Added some responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -6,34 +6,34 @@ import ThemeSwitcher from "./ThemeSwitcher"
 
 function App() {
   return (
-    <div className='w-screen h-screen font-montserrat bg-neutral-100 text-slate-900 dark:bg-slate-800 dark:text-indigo-100 flex'>
-      <div className='flex shadow-xl shadow-black'>
-        <div className="p-5 flex-col flex justify-center items-center bg-neutral-200 dark:bg-slate-800 dark:shadow-sm dark:shadow-gray-600">
-          <span className="material-symbols-outlined p-2 my-1 text-3xl font-light rounded-full cursor-pointer aspect-square flex justify-center align-center
+    <div className='w-screen h-screen font-montserrat bg-neutral-100 text-slate-900 dark:bg-slate-800 dark:text-indigo-100 flex sm:flex-row flex-col-reverse'>
+      <div className='sm:flex shadow-xl shadow-black'>
+        <div className="sm:p-2 p-1 sm:flex-col flex justify-center items-center bg-neutral-200 dark:bg-slate-800 dark:shadow-sm dark:shadow-gray-600">
+          <span className="material-symbols-outlined px-2 m-1 text-3xl font-light rounded-full cursor-pointer aspect-square flex justify-center align-center items-center
          dark:hover:bg-slate-700 hover:bg-neutral-100 transition-all duration-300">
             home
           </span>
-          <span className="material-symbols-outlined p-2 my-1 text-3xl font-light rounded-full cursor-pointer aspect-square flex justify-center align-center
+          <span className="material-symbols-outlined px-2 m-1 text-3xl font-light rounded-full cursor-pointer aspect-square flex justify-center align-center items-center
          dark:bg-slate-700 bg-neutral-100 transition-all duration-300">
             forum
           </span>
-          <span className="material-symbols-outlined p-2 my-1 text-3xl font-light rounded-full cursor-pointer aspect-square flex justify-center align-center
+          <span className="material-symbols-outlined px-2 m-1 text-3xl font-light rounded-full cursor-pointer aspect-square flex justify-center align-center items-center
          dark:hover:bg-slate-700 hover:bg-neutral-100 transition-all duration-300">
             settings
           </span>
-          <div className="absolute bottom-4">
+          <div className="sm:absolute sm:bottom-4 hidden sm:block">
             <ThemeSwitcher />
           </div>
         </div>
-        <div className="p-5 h-full w-80">
-          <div className="flex justify-between align-center items-center mb-10">
-            <p className="text-2xl">Chats</p>
-            <span className="material-symbols-outlined text-3xl font-light rounded-full cursor-pointer aspect-square flex justify-center align-center ml-6">
+        <div className="p-5 h-full w-72 hidden sm:block">
+          <div className="flex justify-between align-center items-center mb-7">
+            <p className="text-xl">Chats</p>
+            <span className="material-symbols-outlined text-2xl font-light rounded-full cursor-pointer aspect-square flex justify-center align-center ml-6">
               search
             </span>
           </div>
-          <div className="border-2 border-neutral-200 hover:bg-neutral-200 shadow-sm dark:border-slate-700 dark:hover:bg-slate-700 transition-all duration-300 p-3 rounded-lg flex cursor-pointer">
-            <div className="w-16 h-12 bg-slate-600 rounded-full"></div>
+          <div className="border-2 border-neutral-200 hover:bg-neutral-200 shadow-sm dark:border-slate-700 dark:hover:bg-slate-700 transition-all duration-300 p-3 rounded-lg flex cursor-pointer text-sm">
+            <div className="w-14 h-10 bg-slate-600 rounded-full"></div>
             <div className='ml-3 w-full'>
               <div className='flex justify-between items-center w-full pr-2'>
                 <div>John Doe</div>
@@ -47,7 +47,7 @@ function App() {
           </div>
         </div>
       </div>
-      <div className="p-5 w-full dark:bg-slate-700 bg-neutral-200">
+      <div className="p-5 w-full h-full dark:bg-slate-700 bg-neutral-200">
         Current chat
       </div>
     </div>
